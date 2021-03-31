112df00f092fa3e6510d118906a41a193ac8daa3
SignedSnow0 <claudiomarchini2003@gmail.com>
2021-03-31 09:56:25 +0200

    Iniziato index

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Pagina internet bellissima</title>
+</head>
+<body>
+    <div class="navbar">
+        <label>Azienda<label>Bellissima</label></label>
+        <button>Acquista</button>
+    </div>
+    <div class="body">
+        <div class="wrapper">
+            <img src="resources/immagini/tmp.jpg" alt="Immagine menu software">
+            <div class="hero">
+                <h1>Analizza le tue sessioni sportive</h1>
+                <h2>migliora le tue prestazioni sul campo</h2>
+                <label>Questo rivoluzionario software permette di analizzare qualsiasi partita registrata e  ottenerne un analisi approfondita sulle  prestazioni di ogni giocatore</label>
+            </div>       
+        </div>
+    </div>
+    <div class="descrizione">
+
+    </div>
+
+    <div class="footer">
+        <ul>
+            <li class="yt">
+                <img src="resources/immagini/youtube.svg">
+                <label>YouTube</label>
+            </li>
+            <li class="insta">
+                <img src="resources/immagini/instagram.svg">
+                <label>Instagram</label>
+            </li>
+            <li class="twitter">
+                <img src="resources/immagini/twitter.svg">
+                <label>Twitter</label>
+            </li>
+            <li class="fb">
+                <img src="resources/immagini/facebook.svg">
+                <label>Facebook</label>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
