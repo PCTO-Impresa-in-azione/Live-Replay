5aab6b37b506a9f67217361885865a8d9065b261
SignedSnow0 <claudiomarchini2003@gmail.com>
2021-04-19 11:04:58 +0200

    Implementato rilevamento oggetti usando yolo

--- src/darknet.py
+++ src/darknet.py
@@ -1,21 +1,25 @@
 #non so perchè si chiami darknet.py, il sito diceva così e io mi fido di lui
-
 from __future__ import division
 
+from torch._C import wait
+from utils import *
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import numpy as np
+import cv2
 
-class EmptyLayer(nn.Module):
-    def __init__(self):
-        super(EmptyLayer, self).__init__()
+def get_test_input():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class DetectionLayer(nn.Module):
-    def __init__(self, anchors):
-        super(DetectionLayer, self).__init__()
-        self.anchors = anchors
+    img = cv2.imread("resources/prova.png")
+    img = cv2.resize(img, (416, 416))
+    img_ = img[:,:,:: - 1].transpose((2,0,1)) # da bgr a rgb
+    img_ = img_[np.newaxis,:,:,:] / 255.0 #aggiunge un canale (per batch) e normalizza i valori   
+    img_ = torch.from_numpy(img_).float().to(device)
+    img_ = Variable(img_)
+    return img_
 
 #ottiene dal file di config un array con tutti i blocks (array di array)
 def parse_cfg(cfgfile):
@@ -40,6 +44,15 @@ def parse_cfg(cfgfile):
 
     blocks.append(block)
     return blocks
+
+class EmptyLayer(nn.Module):
+    def __init__(self):
+        super(EmptyLayer, self).__init__()
+
+class DetectionLayer(nn.Module):
+    def __init__(self, anchors):
+        super(DetectionLayer, self).__init__()
+        self.anchors = anchors
 
 #crea moduli pytorch da un array di blocchi
 def create_modules(blocks):
@@ -68,7 +81,7 @@ def create_modules(blocks):
             stride = int(x["stride"]) #dimensione di un elemento
 
             if padding:
-                pad = (kernel_size - 1)
+                pad = (kernel_size - 1) // 2
             else:
                 pad = 0
 
@@ -78,7 +91,7 @@ def create_modules(blocks):
 
             #batch normalize
             if batch_normalize:
-                bn = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias = bias)
+                bn = nn.BatchNorm2d(filters)
                 module.add_module("batch_norm{0}".format(index), bn)
 
             #activation type (linear o leakyReLu)
@@ -88,7 +101,7 @@ def create_modules(blocks):
 
         elif (x["type"] == "upsample"):
             stride = int(x["stride"])
-            upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+            upsample = nn.Upsample(scale_factor = 2, mode = "nearest")
             module.add_module("upsample_{0}".format(index), upsample)
 
         elif (x["type"] == "route"):
@@ -128,12 +141,131 @@ def create_modules(blocks):
             detection = DetectionLayer(anchors)
             module.add_module("detection_{0}".format(index), detection)
 
+        if(torch.cuda.is_available()):
+            module = module.cuda()
         module_list.append(module) #alla fine del ciclo aggiungo il modulo appena creato
         prev_filters = filters
         output_filters.append(filters)
 
     return(net_info, module_list)
-    
-blocks = parse_cfg("cfg/yoloNetwork.cfg") #ottengo e sainifico i dati
-print(create_modules(blocks)) #dai dati costruisco il modello di una rete neurale
+
+
+class Darknet(nn.Module):
+    def __init__(self, cfgfile):
+        super(Darknet, self).__init__()
+        self.blocks = parse_cfg(cfgfile)
+        self.net_info, self.module_list = create_modules(self.blocks)
+
+    #non cambiare la firma, overloading
+    #definisce cosa fare tra un nodo e un altro
+    def forward(self, x, CUDA):
+        modules = self.blocks[1:]
+        outputs = {} #tensor usato nel ciclo precedente
+
+        write = 0 #indica che abbiamo trovato il primo oggetto e possiamo quindi concaterare gli altri rivelamenti al primo tensor
+        for i, module in enumerate(modules):
+            module_type = (module["type"])
+
+            if (module_type == "convolutional" or module_type == "upsample"): #se il modulo richiede un operazione lo eseguo
+                x = self.module_list[i](x)
             
+            elif module_type == "route":
+                layers = module["layers"]
+                layers = [int(a) for a in layers]
+
+                if (layers[0]) > 0:
+                    layers[0] = layers[0] - i
+                if (len(layers) == 1):
+                    x = outputs[i + (layers[0])]
+                else:
+                    if (layers[1] > 0):
+                        layers[1] = layers[1] - i
+
+                    map1 = outputs[i + layers[0]]
+                    map2 = outputs[i + layers[1]]
+                    x = torch.cat((map1, map2), 1)
+            
+            elif (module_type == "shortcut"):
+                from_ = int(module["from"])
+                x = outputs[i - 1] + outputs[i + from_]
+
+            elif (module_type == "yolo"):
+                anchors = self.module_list[i][0].anchors
+                inp_dim = int(self.net_info["height"])
+                num_classes = int(module["classes"])
+
+                x = x.data
+                x = predict_transform(x, inp_dim, anchors, num_classes, CUDA)
+                if not write:
+                    detections = x
+                    write = 1
+                else:
+                    detections = torch.cat((detections, x), 1)
+
+            outputs[i] = x
+
+        return detections
+
+    def load_weights(self, weightfile):
+        fp = open(weightfile, "rb")
+
+        header = np.fromfile(fp, dtype = np.int32, count = 5) #i primi 5 int costituiscono l`header
+        self.header = torch.from_numpy(header)
+        self.seen = self.header[3]
+
+        weights = np.fromfile(fp, dtype = np.float32) #il resto sono i pesi
+
+        ptr = 0
+        for i in range(len(self.module_list)):
+            module_type = self.blocks[i + 1]["type"]
+
+            if (module_type == "convolutional"):
+                model = self.module_list[i]
+                try:
+                    batch_normalize = int(self.blocks[i + 1]["batch_normalize"])
+                except:
+                    batch_normalize = 0
+
+                conv = model[0]
+
+                if batch_normalize: #il layout dei pesi cambia se il nodo ha il flag batch_normalize https://blog.paperspace.com/content/images/2018/04/wts-1.png
+                    bn = model[1]
+                    num_bn_biases = bn.bias.numel()
+
+                    bn_biases = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_weights = torch.from_numpy(weights[ptr: ptr + num_bn_biases])
+                    ptr  += num_bn_biases
+
+                    bn_running_mean = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_running_var = torch.from_numpy(weights[ptr:ptr + num_bn_biases])
+                    ptr += num_bn_biases
+
+                    bn_biases = bn_biases.view_as(bn.bias.data)
+                    bn_weights = bn_weights.view_as(bn.weight.data)
+                    bn_running_mean = bn_running_mean.view_as(bn.running_mean)
+                    bn_running_var = bn_running_var.view_as(bn.running_var)
+
+                    bn.bias.data.copy_(bn_biases)
+                    bn.weight.data.copy_(bn_weights)
+                    bn.running_mean.copy_(bn_running_mean)
+                    bn.running_var.copy_(bn_running_var)
+                else:
+                    num_biases = conv.bias.numel()
+
+                    conv_biases = torch.from_numpy(weights[ptr: ptr + num_biases])
+                    ptr = ptr + num_biases
+
+                    conv_biases = conv_biases.view_as(conv.bias.data)
+
+                    conv.bias.data.copy_(conv_biases)
+
+                num_weights = conv.weight.numel()
+                conv_weights = torch.from_numpy(weights[ptr: ptr + num_weights])
+                ptr += num_weights
+
+                conv_weights = conv_weights.view_as(conv.weight.data)
+                conv.weight.data.copy_(conv_weights)
