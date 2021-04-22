1fbc76885b7fa375379940bbfbaac3d769b16b49
MarcoVincenzi03 <marcovincenzi2003@gmail.com>
2021-04-22 11:06:44 +0200

    push, i push non cambiano mai

--- src/prova.py
+++ src/prova.py
@@ -3,19 +3,16 @@ import numpy as np
 
 # Load Yolo
 print("LOADING YOLO")
-net = cv2.dnn.readNet("yolov3.weights", "yoloNetwork.cfg")
+net = cv2.dnn.readNet("cfg/yolov3.weights", "cfg/yoloNetwork.cfg")
 #save all the names in file o the list classes
 classes = []
-with open("coco.names", "r") as f:
+with open("data/coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 #get layers of the network
 layer_names = net.getLayerNames()
 #Determine the output layer names from the YOLO model 
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 print("YOLO LOADED")
-
-
-
 
 video_capture = cv2.VideoCapture(0)
 while True:
