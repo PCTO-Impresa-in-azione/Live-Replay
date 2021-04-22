8ce674be7b4cbe2a54cb31a137d19c97762d2422
Luca Vignali <lucavigno2003@gmail.com>
2021-04-22 10:04:49 +0200

    a

--- src/prova.py
+++ src/prova.py
@@ -1,3 +1,22 @@
+import cv2
+import numpy as np
+
+# Load Yolo
+print("LOADING YOLO")
+net = cv2.dnn.readNet("yolov3.weights", "yoloNetwork.cfg")
+#save all the names in file o the list classes
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+#get layers of the network
+layer_names = net.getLayerNames()
+#Determine the output layer names from the YOLO model 
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+print("YOLO LOADED")
+
+
+
+
 video_capture = cv2.VideoCapture(0)
 while True:
     # Capture frame-by-frame
