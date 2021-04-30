1786d98267bc340eb82e5fd07658f11ee12be65b
Luca Vignali <lucavigno2003@gmail.com>
2021-04-30 11:25:19 +0200

    provate voi

--- src/rilevazione.py
+++ src/rilevazione.py
@@ -35,9 +35,44 @@ def LoadImage(sender, data):
                     break
                 height, width, channels = img.shape
 
-                blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    #converting into hsv image
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+	#green range
+lower_green = np.array([40,40, 40])
+upper_green = np.array([70, 255, 255])
+	#blue range
+lower_blue = np.array([110,50,50])
+upper_blue = np.array([130,255,255])
+
+	#Red range
+lower_red = np.array([0,31,255])
+upper_red = np.array([176,255,255])
+
+	#white range
+lower_white = np.array([0,0,0])
+upper_white = np.array([0,0,255])
+
+    #Define a mask ranging from lower to uppper
+mask = cv2.inRange(hsv, lower_green, upper_green)
+    #Do masking
+res = cv2.bitwise_and(img, img, mask=mask)
+	#convert to hsv to gray
+res_bgr = cv2.cvtColor(res,cv2.COLOR_HSV2BGR)
+res_gray = cv2.cvtColor(res,cv2.COLOR_BGR2GRAY)
+
+    #Defining a kernel to do morphological operation in threshold image to 
+    #get better output.
+kernel = np.ones((13,13),np.uint8)
+thresh = cv2.threshold(res_gray,127,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+	
+
+    #find contours in threshold image     
+im2,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+blob = cv2.dnn.blobFromImage(img, 1 / 255.0, (416, 416), swapRB=True, crop=False)
                     #Detecting objects
-                net.setInput(blob)
+net.setInput(blob)
                 outs = net.forward(output_layers)
 
                 # Showing informations on the screen
@@ -47,6 +82,7 @@ def LoadImage(sender, data):
                 for out in outs:
                     for detection in out:
                         scores = detection[5:]
+                    for detection in out:
                         class_id = np.argmax(scores)
                         confidence = scores[class_id]
                         if confidence > 0.7:
